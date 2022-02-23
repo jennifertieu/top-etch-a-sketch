5c75c9d904f03418e68a68577670eca7cc46e020
Jennifer Tieu <jennifer.tieuu@gmail.com>
2022-02-22 22:44:01 -0600

    create grid

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 "use strict"
 
+let gridNumber = 16;
+
+function createGrid(number) {
+    for (let i = 0; i <= number; i++) {
+        let div = document.createElement("div");
+        div.classList.add("grid-box")
+        document.querySelector(".grid-container").appendChild(div);
+    }
+}
+
+createGrid(gridNumber);
