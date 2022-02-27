6a77880adef8326fc02d3df4ba98cd91ad2853a4
Jennifer Tieu <jennifer.tieuu@gmail.com>
2022-02-26 22:06:35 -0600

    move grid template and columns setting out of loop

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 
 function createGrid(number) {
     const gridContainer = document.querySelector(".grid-container");
+    gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
     for (let i = 1; i <= number*number; i++) {
         let div = document.createElement("div");
         div.classList.add("grid-box")
         gridContainer.appendChild(div);
-        gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
-        gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
     }
     document.querySelector(".grid-dimensions").textContent = `${number}x${number}`;
 }
