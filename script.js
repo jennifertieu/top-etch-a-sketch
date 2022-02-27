c13fb65fda47498af79b3a1ce4be5930b9aa8f19
Jennifer Tieu <jennifer.tieuu@gmail.com>
2022-02-26 20:31:17 -0600

    added grid functions, event listeners, and styling

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
 "use strict"
 
-let gridNumber = 16;
-
 function createGrid(number) {
-    for (let i = 0; i <= number; i++) {
+    const gridContainer = document.querySelector(".grid-container");
+    for (let i = 1; i <= number*number; i++) {
         let div = document.createElement("div");
         div.classList.add("grid-box")
-        document.querySelector(".grid-container").appendChild(div);
+        gridContainer.appendChild(div);
+        gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
     }
+    document.querySelector(".grid-dimensions").textContent = `${number}x${number}`;
 }
 
+function changeColor(event){
+    event.target.style.backgroundColor = "black";
+}
+
+function resetGrid(){
+    let num = prompt("Please enter grid size less than or equal to 100:");
+    if (num === undefined || num === null || num === "") {
+        return;
+    }
+
+    while (num > 100){
+        num = prompt("Number too big! Please enter grid size less than or equal to 100:");
+    }
+
+    const gridContainer = document.querySelector(".grid-container");
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    createGrid(num);
+    let gridBoxList = document.querySelectorAll(".grid-box");
+    gridBoxList.forEach(gridBox => {gridBox.addEventListener("mouseover", changeColor)});
+}
+
+function clearGrid(){
+    let gridBoxList = document.querySelectorAll(".grid-box");
+    gridBoxList.forEach(gridBox => {gridBox.style.backgroundColor = null});
+}
+
+let gridNumber = 16;
 createGrid(gridNumber);
+let gridBoxList = document.querySelectorAll(".grid-box");
+gridBoxList.forEach(gridBox => {gridBox.addEventListener("mouseover", changeColor)});
